66e733024b03930b0241d5a66fd56098452b2483
Okayama-cloud <ukyokan@yahoo.com>
2025-02-25 18:03:28 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,1 @@
-let slideIndex = 0;
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.opacity = "0";  
-    }
-    
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }    
-    slides[slideIndex - 1].style.opacity = "1";  
-
-    setTimeout(showSlides, 5000); // 5秒ごとに切り替え
-}
-
-showSlides(); // スライドショーを開始
