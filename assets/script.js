fae849435e667c653edacec00825c5a496cb4b89
Okayama-cloud <ukyokan@yahoo.com>
2025-02-25 17:53:17 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,17 @@ let slideIndex = 0;
 
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
+    
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+    
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) { slideIndex = 1 }    
     slides[slideIndex - 1].style.display = "block";  
+
     setTimeout(showSlides, 5000); // Change slide every 5 seconds
 }
 
-showSlides(); // Initial call
+showSlides(); // Start the slideshow
+
